885b7ff7016c2621e73dff107c93a5ee8168f033
Tom Morris <tom@tommorris.org>
2022-03-04 19:51:18 +0000

    Add GitHub module

---
+++ src/github.cr
@@ -0,0 +1,12 @@
+module GitHub
+  extend self
+
+  def get_current_pr
+    current_pr = JSON.parse(`gh pr view --json body,title,baseRefName`)
+    title = current_pr["title"].as_s
+    body = current_pr["body"].as_s
+    base = current_pr["baseRefName"].as_s
+
+    return { current_pr, title, body, base }
+  end
+end
