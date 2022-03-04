08441b1d1710ba22fa0b442ed6a1d92529cec57a
Tom Morris <tom@tommorris.org>
2022-03-04 16:38:02 +0000

    Switch to main function

--- src/branchsync.cr
+++ src/branchsync.cr
@@ -35,49 +35,52 @@ def pandoc_rewrap(str)
   return output
 end
 
-current_pr = JSON.parse(`gh pr view --json body,title,baseRefName`)
-title = current_pr["title"].as_s
-body = current_pr["body"].as_s
-base = current_pr["baseRefName"].as_s
+def main
+  current_pr = JSON.parse(`gh pr view --json body,title,baseRefName`)
+  title = current_pr["title"].as_s
+  body = current_pr["body"].as_s
+  base = current_pr["baseRefName"].as_s
 
-commits_on_this_branch = `git log --pretty=oneline #{base}..HEAD`.split("\n").select { |i| i != "" }
+  commits_on_this_branch = `git log --pretty=oneline #{base}..HEAD`.split("\n").select { |i| i != "" }
 
-if commits_on_this_branch.size != 1
-  puts "Branch syncing only works with one single commit."
-  exit 1
-end
-
-commit_sha = commits_on_this_branch[0].split(" ")[0]
-rev = `git rev-list --format=%B --max-count=1 #{commit_sha}`.split("\n")
-
-args = [] of String
-
-new_title = rev[1].strip
-if new_title != title
-  args << "-t '#{esc_squot(new_title)}'"
-  `gh pr edit -t '#{esc_squot(new_title)}'`
-  puts "Changed PR title"
-end
-
-new_body = nil
-if rev.size > 2
-  body_unprocessed = rev[2..].join("\n").strip
-  # new_body = body_unprocessed.split(/\n\n/).map {|i| i.gsub(/\n/, " ").strip.squeeze(" ") }.join("\n\n").strip.squeeze(" ")
-  new_body = pandoc_rewrap(body_unprocessed)
-end
-
-if DEBUG
-  puts "Existing body..."
-  puts body
-  puts "New body..."
-  puts new_body
-end
-
-if !(new_body.nil?) && new_body != body
-  puts "Syncing new bodies" if DEBUG
-  tempfile = File.tempfile("github_pr_sync") do |file|
-    file.print(new_body)
+  if commits_on_this_branch.size != 1
+    puts "Branch syncing only works with one single commit."
+    exit 1
   end
-  run("gh", ["pr", "edit", "-F", tempfile.path])
-  tempfile.delete
+
+  commit_sha = commits_on_this_branch[0].split(" ")[0]
+  rev = `git rev-list --format=%B --max-count=1 #{commit_sha}`.split("\n")
+
+  args = [] of String
+
+  new_title = rev[1].strip
+  if new_title != title
+    args << "-t '#{esc_squot(new_title)}'"
+    `gh pr edit -t '#{esc_squot(new_title)}'`
+    puts "Changed PR title"
+  end
+
+  new_body = nil
+  if rev.size > 2
+    body_unprocessed = rev[2..].join("\n").strip
+    new_body = pandoc_rewrap(body_unprocessed)
+  end
+
+  if DEBUG
+    puts "Existing body..."
+    puts body
+    puts "New body..."
+    puts new_body
+  end
+
+  if !(new_body.nil?) && new_body != body
+    puts "Syncing new bodies" if DEBUG
+    tempfile = File.tempfile("github_pr_sync") do |file|
+      file.print(new_body)
+    end
+    run("gh", ["pr", "edit", "-F", tempfile.path])
+    tempfile.delete
+  end
 end
+
+main()
